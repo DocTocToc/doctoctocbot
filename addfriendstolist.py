351ed8f0ba2cf7155c15034e8a8edeeffec688e7
Jerome Pinguet <jerome@jerome.cc>
2017-07-10 23:31:58 +0200

    add new files, update .gitignore

---
+++ addfriendstolist.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# add friends (whitelist) to privat Twitter list "docs"
+# bot user_id=881706502939185152
+import os, configparser, tweepy, inspect, pickle, time
+
+slug = "docs"
+bot_id=881706502939185152
+
+path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+file = os.path.join(path, "friends")
+print "friends file:", file
+
+# read config
+config = configparser.SafeConfigParser()
+config.read(os.path.join(path, "config"))
+
+# your hashtag or search query and tweet language (empty = all languages)
+tweetLanguage = config.get("settings", "tweet_language")
+
+# create bot
+auth = tweepy.OAuthHandler(config.get("twitter", "consumer_key"), config.get("twitter", "consumer_secret"))
+auth.set_access_token(config.get("twitter", "access_token"), config.get("twitter", "access_token_secret"))
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+with open(file, mode='r') as f:
+    friends = [line.rstrip('\n') for line in f]
+
+lists = api.lists_all()
+print "List of the lists \n"
+for list in lists:
+    print "list: ", list.slug, list.id
+
+for friend in friends:
+    api.add_list_member(owner_id=bot_id, slug=slug, user_id=int(friend))
+    print "The user {} was added to list {}.".format(friend,slug)
---
+++ occurence.py
@@ -0,0 +1,18 @@
+import re
+import string
+import codecs
+import io
+
+frequency = {}
+with io.open('bio', encoding='utf-8', mode='r') as f:
+    text_string = f.read().lower()
+match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
+
+for word in match_pattern:
+    count = frequency.get(word,0)
+    frequency[word] = count + 1
+
+frequency_list = frequency.keys()
+
+for words in frequency_list:
+    print words, frequency[words]

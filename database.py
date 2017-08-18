e9d3c1fb0e8c331e41a699998b238654856743ae
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2017-08-18 17:21:30 +0200

    Create 'sessionmaker'

--- database.py
+++ database.py
@@ -8,6 +8,13 @@ Author: Anatole Hanniet.
 License: Mozilla Public License (2.0), see 'LICENSE' for details.
 """
 
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+# Parent class for objects to persist in database
 Base = declarative_base()
+
+# Database session factory
+engine = create_engine('DATABASE_URL')
+Session = sessionmaker(bind=engine)

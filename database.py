db1f2d98f72a3c50d72d69f2563952278c443600
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2017-08-18 13:08:28 +0200

    Create the database object

--- database.py
+++ database.py
@@ -7,3 +7,7 @@ Implement the database.
 Author: Anatole Hanniet.
 License: Mozilla Public License (2.0), see 'LICENSE' for details.
 """
+
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()

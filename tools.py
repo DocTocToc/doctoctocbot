83b03baf5a9dafea49c289b6adddf57236655b6c
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2017-08-18 17:42:30 +0200

    Create 'load_configuration' function
    The 'load_configuration' load a configuration file (INI structure) and return a ConfigParser() instance filled with config values.

--- tools.py
+++ tools.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from configparser import ConfigParser
+
+
+def load_configuration(source):
+    """Load an INI configuration file."""
+    configuration = ConfigParser()
+    configuration.read(str(source))
+    return configuration

8b7fb792cc573fb649ebfd58089fc63157a854d9
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2017-08-18 13:13:28 +0200

    Create the User class

--- users.py
+++ users.py
@@ -7,3 +7,15 @@ Implement model and storage for bot's contributors.
 Author: Anatole Hanniet.
 License: Mozilla Public License (2.0), see 'LICENSE' for details.
 """
+
+from sqlalchemy import Column
+from sqlalchemy import Integer, String
+from database import Base
+
+
+class User(Base):
+    """A user of the service."""
+
+    __tablename__ = 'user'
+    identifier = Column(Integer, primary_key=True)
+    name = Column(String(length=250), nullable=True, unique=True, index=True)

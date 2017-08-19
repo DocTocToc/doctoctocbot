c201823bb4e64b1a1a2043c6a693f8b813e1ef67
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2017-08-19 16:11:18 +0200

    Create 'connect' function
    Connect to the Twitter API and return an API object that interface the Twitter API in python.

--- tools.py
+++ tools.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from configparser import ConfigParser
+from tweepy import API, OAuthHandler
 
 
 def load_configuration(source):
@@ -9,3 +10,10 @@ def load_configuration(source):
     configuration = ConfigParser()
     configuration.read(str(source))
     return configuration
+
+
+def connect(app_token, app_secret, access_token, access_secret):
+    """Connect to the Twitter API."""
+    auth = OAuthHandler(app_token, app_secret)
+    auth.set_access_token(access_token, access_secret)
+    return API(auth)

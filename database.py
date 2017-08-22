efe0d03e34a7353021d0153d0f521f15f7db66dd
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2017-08-22 19:28:18 +0200

    Create a session factory 'Session'
    Create a session factory 'Session' for creating session objects to query the database. Append the necessary configuration parameters to the sample configuration file 'config.sample'.

--- database.py
+++ database.py
@@ -11,7 +11,14 @@ License: Mozilla Public License (2.0), see 'LICENSE' for details.
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from utils import load_config
 
 
-# Parent class for every class which need to be persist in the database.
+# Parent class for every class which objects need to be persist in the
+# database.
 Base = declarative_base()
+
+# Create a session factory
+config = load_config()
+engine = create_engine(config.get('DATABASE', 'URL'))
+Session = sessionmaker(bind=engine)

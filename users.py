077c200f7e5fa85a1d6bf0d6c1ea7bac8b49f349
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2017-08-22 19:32:16 +0200

    Create the 'User' class

--- users.py
+++ users.py
@@ -7,3 +7,19 @@ Implement the user model.
 Author: Anatole Hanniet.
 License: Mozilla Public License (2.0), see 'LICENSE' for details.
 """
+
+
+from sqlalchemy import BigInteger, Column, String
+from database import Base
+
+
+class User(Base):
+    """A user of the application."""
+
+    __tablename__ = 'users'
+    identifier = Column(BigInteger, primary_key=True)
+    name = Column(String(length=250), index=True)
+
+    def __repr__(self):
+        """Return a unique string representation of the user."""
+        return '<User #{0}: {1}>'.format(self.identifier, self.name)

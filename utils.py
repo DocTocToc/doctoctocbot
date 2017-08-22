9f16fbb9eaf2bf2bb36770d76a465cdc12aa70c8
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2017-08-22 19:11:18 +0200

    Add docstring to 'utils' module

--- utils.py
+++ utils.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+u"""
+Implement usefull tools for the project.
+
+Author: Anatole Hanniet.
+License: Mozilla Public License (2.0), see 'LICENSE' for details.
+"""

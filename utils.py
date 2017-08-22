7f817a651bdab0848eef1846d582c4dc0d67a257
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2017-08-22 19:14:00 +0200

    Create the 'load_config' function
    The 'load_config' function load an INI-like configuration file and return a 'configparser.ConfigParser' object.

--- utils.py
+++ utils.py
@@ -7,3 +7,25 @@ Implement usefull tools for the project.
 Author: Anatole Hanniet.
 License: Mozilla Public License (2.0), see 'LICENSE' for details.
 """
+
+from configparser import ConfigParser
+from pathlib import Path
+
+
+ROOT_PATH = Path(__file__).resolve().parent
+
+
+def load_config(source=ROOT_PATH.joinpath('config.ini')):
+    """
+    Load a configuration file.
+
+    - source, 'pathlib.Path' object or string representing a path. If not
+    provided default location and name are used for the configuration file.
+    The default location is the project directory, the default name is
+    'config.ini'.
+
+    Return a 'configparser.ConfigParser' instance.
+    """
+    config = ConfigParser()
+    config.read(str(source))
+    return config

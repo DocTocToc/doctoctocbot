ce8d29f47ece0209238c4826360dc90dcab88784
Jerome Pinguet <jerome@jerome.cc>
2018-09-25 01:04:08 +0300

    use env variable to switch between test (debug) and production settings

--- conf/cfg.py
+++ conf/cfg.py
@@ -3,8 +3,16 @@ import inspect
 import json
 
 def getConfig():                                                        
-    "get config object"                                               
-    configfilename = "configtest.json"
+    """
+    Get config object.
+    To use production configuration file, export environement variable
+    BOT_DEBUG=False. Without this environment variable, defaults to configtest.json.
+    """
+    DEBUG = bool(os.environ.get('BOT_DEBUG', True))
+    if DEBUG:
+        configfilename = "configtest.json"
+    else:
+        configfilename = "configprod.json"
     path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     configfilepath = os.path.join(path, configfilename)
     #config = configparser.SafeConfigParser()

541c60051e5da30264c29a74b618bef8972934e3
Jerome Pinguet <jerome@jerome.cc>
2019-01-06 04:07:04 +0100

    use a password for redis

--- src/doctocnet/settings/base.py
+++ src/doctocnet/settings/base.py
@@ -210,6 +210,7 @@ CACHES = {
         "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            #'PASSWORD': 'a_better_password_please',
         }
     }
 }

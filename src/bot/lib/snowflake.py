5b67fbc4593b806dc1f3d4d3b5b9b1083793c4b8
Jerome Pinguet <jerome@jerome.cc>
2019-02-22 19:59:48 +0100

    add module to get creation time of status from statusid

---
+++ src/bot/lib/snowflake.py
@@ -0,0 +1,37 @@
+# Twitter Snowflake ID to timestamp (and back)
+# https://github.com/client9/snowflake2time/
+# Nick Galbreath @ngalbreath nickg@client9.com
+# Public Domain -- No Copyright -- Cut-n-Paste!
+#  but be kind and give credit
+#
+
+# Python by default has unlimited integer precision, so this is mostly
+# an exercise in date/time/UTC conversion
+
+import time
+import calendar
+
+def str2utc(s):
+    # parse twitter time string into UTC seconds, unix-style
+    # python's bizarro world of dates, times and calendars
+    return calendar.timegm(time.strptime(s, "%a %b %d %H:%M:%S +0000 %Y"))
+
+def utc2snowflake(stamp):
+    return (int(round(stamp * 1000)) - 1288834974657) << 22
+
+def snowflake2utc(sf: int) -> int:
+    """Return epoch (unit: second)
+    sf: statusid (bigint)
+    """
+    return ((sf >> 22) + 1288834974657) / 1000.0
+
+def str2utcms(s):
+    return 1000 * calendar.timegm(time.strptime(s, "%a %b %d %H:%M:%S +0000 %Y"))
+
+def snowflake2utcms(sf):
+    return ((sf >> 22) + 1288834974657)
+
+# really is the best way to get utc timestamp?
+#   (minus changing your box to be UTC)
+def utcnow():
+    calendar.timegm(datetime.datetime.utcnow().timetuple())
---
+++ src/bot/lib/test_snowflake.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Twitter Snowflake ID to timestamp (and back)
+# https://github.com/client9/snowflake2time/
+# Nick Galbreath @ngalbreath nickg@client9.com
+# Public Domain -- no copyright -- but be kind and give credit
+#
+
+import unittest
+
+from .snowflake import *
+class SnowflakeTest(unittest.TestCase):
+
+    def test_str2utc(self):
+        stamp = str2utc("Mon May 21 22:16:35 +0000 2012")
+        self.assertEquals(stamp, 1337638595)
+
+    def test_snowflake2utc(self):
+        utc = snowflake2utc(204697221847986177)
+        self.assertEquals(int(utc), int(1337638595.44))
+
+    def test_snowflake2utcms(self):
+        utc = snowflake2utcms(204697221847986177)
+        # danger floating point maths
+        self.assertAlmostEqual(utc, 1337638595436)
+
+    def test_diff(self):
+       diff = snowflake2utcms(204697221847986177) - str2utcms("Mon May 21 22:16:35 +0000 2012")
+       self.assertEquals(diff, 436)
+
+if __name__ == '__main__':
+    unittest.main()

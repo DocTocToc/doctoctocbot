849ddd16a6efed6b5f7c185773b7500d0c715614
Jerome Pinguet <jerome@jerome.cc>
2019-02-22 20:01:14 +0100

    improve admin interface

--- src/timeline/admin.py
+++ src/timeline/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Status
 
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('statusid', 'status_url_tag',)
+    search_fields = ['statusid',]
+    fields = ('statusid', 'status_url_tag', 'json',)
+    readonly_fields = ('statusid', 'status_url_tag', 'json',)
 
-admin.site.register(Status)
+admin.site.register(Status, StatusAdmin)

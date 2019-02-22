6a954361059b4979360d2dd83dd42336a73cd51e
Jerome Pinguet <jerome@jerome.cc>
2019-02-22 18:56:13 +0100

    include retweets in bot timeline

--- src/bot/bin/timeline.py
+++ src/bot/bin/timeline.py
@@ -5,9 +5,9 @@ from django.middleware.csrf import get_token
 
 from timeline.models import add_status_tl, Status
 
-def get_timeline():
+def get_timeline_with_rts():
     API = get_api()
-    return API.user_timeline()
+    return API.user_timeline(include_rts=True)
 
 def get_timeline_id_lst(n=None) -> List:
     """
@@ -18,5 +18,5 @@ def get_timeline_id_lst(n=None) -> List:
             in Status.objects.all().filter(json__contains={'in_reply_to_status_id': None})][:n]
 
 def record_timeline():
-    for status in get_timeline():
+    for status in get_timeline_with_rts():
         add_status_tl(status)

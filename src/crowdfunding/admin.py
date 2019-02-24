e8071de1a3dc53ee4e642a8be87c51f6cdab6c6b
Jerome Pinguet <jerome@jerome.cc>
2019-02-24 06:44:52 +0100

    improve crowdfunding admin

--- src/crowdfunding/admin.py
+++ src/crowdfunding/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import Project, ProjectInvestment
 
+class ProjectInvestmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', 'email', 'pledged', 'paid', 'datetime', 'public',)
+
 admin.site.register(Project)
-admin.site.register(ProjectInvestment)
+admin.site.register(ProjectInvestment, ProjectInvestmentAdmin)

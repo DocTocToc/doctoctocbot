7ba0025a1a2cb2f6db74ae582a20ab3c4fae1190
Jerome Pinguet <jerome@jerome.cc>
2019-04-23 18:37:37 +0200

    celery tasks logging WIP

--- src/doctocnet/celery.py
+++ src/doctocnet/celery.py
@@ -1,15 +1,19 @@
 from __future__ import absolute_import
 import os
+import logging
 from celery import Celery
 from celery.schedules import crontab
+from celery.signals import after_setup_logger, after_setup_task_logger
 
 from django.conf import settings
 
+from celery.utils.log import get_task_logger
+logger = get_task_logger(__name__)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'doctocnet.settings.development')
 
-app = Celery('quick_publisher')
+app = Celery('doctocnet')
 app.config_from_object('django.conf:settings')
-
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
@@ -69,3 +73,30 @@ app.conf.beat_schedule = {
         'args': None,
         },
     }
+
+@after_setup_task_logger.connect
+def after_setup_celery_task_logger(logger, **kwargs):
+    """ This function sets the 'celery.task' logger handler and formatter """
+    create_celery_logger_handler(logger, True)
+
+
+@after_setup_logger.connect
+def after_setup_celery_logger(logger, **kwargs):
+    """ This function sets the 'celery' logger handler and formatter """
+    create_celery_logger_handler(logger, False)
+
+def create_celery_logger_handler(logger, propagate):
+    # 209715200 is 1024 * 1024 * 200 or 200 MB, same as in settings
+    celery_handler = RotatingFileHandler(
+        settings.CELERY_LOG_FILE,
+        maxBytes=209715200,
+        backupCount=10
+    )
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    #celery_formatter = jsonlogger.JsonFormatter(settings.LOGGING['formatters']['json']['format'])
+    celery_handler.setFormatter(formatter)
+
+    logger.addHandler(celery_handler)
+    logger.logLevel = settings.LOG_LEVEL
+    logger.propagate = propagate
--- src/moderation/tasks.py
+++ src/moderation/tasks.py
@@ -12,34 +12,7 @@ from django.conf import settings
 from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
-from celery.signals import after_setup_logger, after_setup_task_logger
 
-@after_setup_task_logger.connect
-def after_setup_celery_task_logger(logger, **kwargs):
-    """ This function sets the 'celery.task' logger handler and formatter """
-    create_celery_logger_handler(logger, True)
-
-
-@after_setup_logger.connect
-def after_setup_celery_logger(logger, **kwargs):
-    """ This function sets the 'celery' logger handler and formatter """
-    create_celery_logger_handler(logger, False)
-
-def create_celery_logger_handler(logger, propagate):
-    # 209715200 is 1024 * 1024 * 200 or 200 MB, same as in settings
-    celery_handler = RotatingFileHandler(
-        settings.CELERY_LOG_FILE,
-        maxBytes=209715200,
-        backupCount=10
-    )
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    #celery_formatter = jsonlogger.JsonFormatter(settings.LOGGING['formatters']['json']['format'])
-    celery_handler.setFormatter(formatter)
-
-    logger.addHandler(celery_handler)
-    logger.logLevel = settings.LOG_LEVEL
-    logger.propagate = propagate
 
 """
 @after_setup_task_logger.connect

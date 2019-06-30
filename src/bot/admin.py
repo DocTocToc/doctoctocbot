ba68ed71be0db8eb76d493995fb4706aa9221153
Jerome Pinguet <jerome@jerome.cc>
2019-07-01 00:19:59 +0200

    add models

--- src/bot/admin.py
+++ src/bot/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 
+from .models import Account, Greet
 
-# Register your models here.
+admin.site.register(Account)
+admin.site.register(Greet)
--- src/bot/models.py
+++ src/bot/models.py
@@ -1,4 +1,33 @@
 from django.db import models
 
 
-# Create your models here.
+class Account(models.Model):
+    userid = models.BigIntegerField(unique=True)
+    username = models.CharField(max_length=15)
+    twitter_consumer_key = models.CharField(max_length=100)
+    twitter_consumer_secret = models.CharField(max_length=100)
+    twitter_access_token = models.CharField(max_length=100)
+    twitter_access_token_secret = models.CharField(max_length=100)
+    active = models.BooleanField(default=False)
+    hashtag = models.ManyToManyField(
+        "conversation.Hashtag",
+        blank=True    
+    )
+    
+    def __str__(self):
+        return self.username
+
+
+class Greet(models.Model):
+    account = models.ForeignKey(
+        Account,
+        on_delete=models.CASCADE,
+    )
+    active = models.BooleanField(default=False)
+    campaign = models.ForeignKey(
+        "messenger.Campaign",
+        on_delete=models.CASCADE,
+    )
+    
+    def __str__(self):
+        return "{} {}".format(self.account.username, self.campaing.name)

3cf2829708a43079ba22297193802e6d116e5959
Jerome Pinguet <jerome@jerome.cc>
2019-08-12 06:56:22 +0200

    remove debug toolbar urls

--- src/doctocnet/urls.py
+++ src/doctocnet/urls.py
@@ -56,11 +56,4 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns

2d5024ddb76b65f17f2788ee9c257043ffab9e0f
Jerome Pinguet <jerome@jerome.cc>
2019-10-09 18:43:01 +0200

    fix moderation migration

--- src/moderation/migrations/0026_auto_20190915_2314.py
+++ src/moderation/migrations/0026_auto_20190915_2314.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('community', '0003_auto_20190909_0306'),
+#        ('community', '0003_auto_20190909_0306'),
         ('moderation', '0025_usercategoryrelationship_community'),
     ]
 

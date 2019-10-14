2881d0f326f8b14c538fe0877341121cb4926d96
Jerome Pinguet <jerome@jerome.cc>
2019-10-14 21:20:57 +0200

    fix active member count

--- src/landing/templatetags/landing_investors.py
+++ src/landing/templatetags/landing_investors.py
@@ -14,6 +14,12 @@ logger = logging.getLogger(__name__)
 
 register = template.Library()
 
+def get_active_member_count(members_userid_lst, followers_userid_lst):
+    if not members_userid_lst or not followers_userid_lst:
+        return 0
+    return len(set(members_userid_lst) & set(followers_userid_lst))
+
+
 @register.inclusion_tag('landing/investor_progress_bar.html', takes_context=True)
 def investor_progress_bar(context):
     """
@@ -26,15 +32,16 @@ def investor_progress_bar(context):
     """
     community = get_community(context)
     investor_count: int = get_total_investor_count(context)
-    members_userid_lst = UserCategoryRelationship.objects.filter(
+    members_userid_qs = UserCategoryRelationship.objects.filter(
         community__in=community.trust.all(),
         category__in=community.membership.all()
         ).distinct('social_user').values_list('social_user__user_id', flat=True)
+    members_userid_lst = list(members_userid_qs)
     logger.info(f"members_userid_lst: {members_userid_lst}")
     bot_userid = community.account.userid
     followers_userid_lst = update_followersids(bot_userid, cached=True)
     logger.info(f"followers_userid_lst {followers_userid_lst}")
-    active_member_count = len(set(list(members_userid_lst)) & set(followers_userid_lst))
+    active_member_count = get_active_member_count(members_userid_lst, followers_userid_lst)
     try:
         percentage = Decimal(investor_count / active_member_count * 100)
         percentage = percentage.quantize(Decimal('.01'), rounding=ROUND_DOWN)
@@ -43,6 +50,6 @@ def investor_progress_bar(context):
         percentage = 0
     return {
         "investor_count": investor_count,
-        "active_member_count": active_member_count,
+        "active_member_count": _active_member_count,
         "percentage": percentage
     }

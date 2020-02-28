356053ef6063d272c36b4eee23154758f00e330e
Jerome Pinguet <jerome@jerome.cc>
2020-02-28 03:59:11 +0100

    debug

--- src/display/tasks.py
+++ src/display/tasks.py
@@ -1,9 +1,12 @@
+import logging
 from celery import shared_task
 from .constants import HOURS
 from conversation.tree.tweet_server import get_tweet
 from .models import create_or_update_webtweet
 from timeline.models import last_retweeted_statusid_lst
 from community.models import Community
+
+logger = logging.getLogger(__name__)
 
 @shared_task
 def handle_scrape_web_timeline(hourdelta=None):
@@ -19,6 +22,7 @@ def handle_scrape_web_timeline(hourdelta=None):
 @shared_task
 def handle_scrape_status(statusid: int):
             tweet = get_tweet(statusid)
+            logger.debug(tweet)
             if tweet is None:
                 return
             create_or_update_webtweet(tweet)

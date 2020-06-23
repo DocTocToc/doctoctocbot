c563fbc31174bf9629615a79c8b80d5de59bebd0
Jerome Pinguet <jerome@jerome.cc>
2020-06-23 20:58:47 +0300

    add autocompletion, search and filter to opt in admin

--- src/optin/admin.py
+++ src/optin/admin.py
@@ -8,7 +8,14 @@ class OptInAdmin(VersionedAdmin):
         'socialuser',
         'authorize',    
     )
-    
+    search_fields = (
+        'socialuser__profile__json',
+    )
+    list_filter = (
+        'option__label',
+        'authorize',
+    )
+    autocomplete_fields = ['socialuser']
     list_display_show_identity = False
     list_display_show_end_date = True
     list_display_show_start_date = True

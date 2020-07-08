8092020da626f9c7de66fe47f366ea0bc11e8e72
Jerome Pinguet <jerome@jerome.cc>
2020-07-08 09:03:23 +0300

    fix handle_incoming_friendship task

--- src/request/tasks.py
+++ src/request/tasks.py
@@ -1,6 +1,7 @@
 import logging
-import datetime
+from datetime import datetime
 import pytz
+from typing import Optional
 from dateutil.relativedelta import relativedelta
 from celery import shared_task
 from community.models import Community
@@ -14,6 +15,123 @@ from autotweet.accept import accept_follower, decline_follower
 from moderation.social import update_social_ids
 
 logger = logging.getLogger(__name__)
+
+"""Create a queue
+"""
+def create_queue(community, uid, bot_screen_name):
+    try:
+        socialmedia = SocialMedia.objects.get(name='twitter')
+    except SocialMedia.DoesNotExist:
+        return
+    socialuser = addsocialuser_from_userid(uid)
+    if not socialuser:
+        return
+    create_update_profile_twitter(
+            socialuser,
+            bot_screen_name=bot_screen_name
+        )
+    try:
+        Queue.objects.create(
+            uid=uid,
+            socialmedia=socialmedia,
+            socialuser=socialuser,
+            community=community,
+            state=Queue.PENDING
+        )
+    except DatabaseError as e:
+        logger.error(f"Database error during Request Queue creation; {e}")
+
+""" Return True if current pending request queue exists
+"""
+def pending_request_exists(community, uid):
+    return Queue.objects.current.filter(
+        uid=uid,
+        community=community,
+        state=Queue.PENDING
+    ).exists()
+
+""" Return True if current accepted request queue exists
+"""
+def accept_request_exists(community, uid):
+    return Queue.objects.current.filter(
+        uid=uid,
+        community=community,
+        state=Queue.ACCEPT
+    ).exists()
+
+""" Return True if time elapsed since last declined request for this
+    community / user pair is less than follow_request_backoff
+"""
+def decline_backoff(community, uid):
+    decline_queues = Queue.objects.filter(
+        uid=uid,
+        community=community,
+        state=Queue.DECLINE
+    )
+    # if a request was declined in the past, check its creation
+    # datetime
+    if decline_queues:
+        latest_q = (
+            decline_queues.latest('version_end_date') or
+            decline_queues.latest('version_start_date')
+        )
+        logger.debug(f"{latest_q=}")
+        latest_start_dt: Optional[datetime] = getattr(
+            latest_q,
+            'version_start_date',
+            None
+        )
+        latest_end_dt: Optional[datetime] = getattr(
+            latest_q,
+            'version_end_date',
+            None
+        )
+        latest_dt: datetime = latest_start_dt or latest_end_dt 
+        backoff_hours = community.follow_request_backoff # hours
+        if not backoff_hours:
+            return
+        backoff_seconds = backoff_hours * 60 * 60
+        delta = datetime.now(pytz.utc) - latest_dt
+        delta_seconds = delta.total_seconds()
+        logger.debug(f"{delta_seconds=}")
+
+        if delta_seconds < backoff_seconds:
+            return True
+
+""" Return True if we should back-off from creating a new Queue
+    because a Queue object for this community / user pair was recently created
+    or deleted. 
+"""
+def backoff(community, uid):
+    queues = Queue.objects.filter(
+                uid=uid,
+                community=community,
+    )
+    if not queues:
+        return
+    latest_start_q: Optional[Queue] = queues.latest('version_start_date')
+    latest_end_q: Optional[Queue] = queues.latest('version_end_date')
+    latest_start_dt: Optional[datetime] = getattr(
+        latest_start_q,
+        'version_start_date',
+        None
+    )
+    latest_end_dt: Optional[datetime] = getattr(
+        latest_end_q,
+        'version_end_date',
+        None
+    )
+    latest = get_latest(
+        latest_start_dt,
+        latest_end_dt
+    )
+    if latest:
+        backoff: int = community.request_queue_backoff * 60 # second
+        delta = datetime.now(pytz.utc) - latest
+        delta_seconds = delta.total_seconds()
+        logger.debug(f"{delta_seconds=}")
+        if delta_seconds < backoff:
+            return True
 
 @shared_task
 def handle_incoming_friendship():
@@ -30,89 +148,15 @@ def handle_incoming_friendship():
             # for this uid less than one hour ago, do not create a new request
             # queue.
             # This gives enough time for accept / decline tasks to be completed
-            queues = Queue.objects.filter(
-                uid=uid,
-                community=community,
-            )
-            latest_end: Queue = queues.latest('version_end_date')
-            latest_start: Queue = queues.latest('version_start_date')
-            latest = get_latest(
-                latest_start.version_start_date,
-                latest_end.version_end_date
-            )
-            if latest:
-                backoff: int = community.request_queue_backoff * 60 # second
-                delta = datetime.datetime.now(pytz.utc) - latest
-                delta_seconds = delta.total_seconds()
-                logger.debug(f"{delta_seconds=}")
-                if delta_seconds < backoff:
-                    continue
-
-            accept_pending_q_exists = Queue.objects.current.filter(
-                uid=uid,
-                community=community,
-                state__in=[Queue.ACCEPT, Queue.PENDING],
-            ).exists()
-
-            if accept_pending_q_exists:
+            if backoff(community, uid):
                 continue
-
-            decline_q = Queue.objects.filter(
-                uid=uid,
-                community=community,
-                state=Queue.DECLINE
-            )
-            # if a request was declined in the past, check its creation
-            # datetime
-            if decline_q:
-                latest = (
-                    decline_q.latest('version_end_date') or
-                    decline_q.latest('version_start_date')
-                )
-                logger.debug(f"{latest=}")
-                latest_dt = latest.version_start_date
-                backoff_hours = community.follow_request_backoff # hours
-                backoff_seconds = backoff_hours * 60 * 60
-                delta = datetime.datetime.now(pytz.utc) - latest_dt
-                delta_seconds = delta.total_seconds()
-                logger.debug(f"{delta_seconds=}")
-
-                if delta_seconds < backoff_seconds:
-                    continue
-
-            """
-            cancel_q_exists = Queue.objects.current.filter(
-                uid=uid,
-                community=community,
-                state=Queue.CANCEL,
-            ).exists()
-            if cancel_q_exists:
+            if decline_backoff(community, uid):
                 continue
-            """
-            try:
-                socialmedia = SocialMedia.objects.get(name='twitter')
-            except SocialMedia.DoesNotExist:
-                return
-            socialuser = addsocialuser_from_userid(uid)
-            if not socialuser:
-                return
-            try:
-                create_update_profile_twitter(
-                    socialuser,
-                    bot_screen_name=bot_screen_name
-                )
-            except:
+            if pending_request_exists(community, uid):
                 continue
-            try:
-                Queue.objects.create(
-                    uid=uid,
-                    socialmedia=socialmedia,
-                    socialuser=socialuser,
-                    community=community,
-                    state=Queue.PENDING
-                )
-            except DatabaseError:
+            if accept_request_exists(community, uid):
                 continue
+            create_queue(community, uid, bot_screen_name)
 
 @shared_task
 def handle_accept_follower_twitter(userid: int, community__id: int):

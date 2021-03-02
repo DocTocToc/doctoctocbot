dc5cbf8679417311b320f98221fa4150657dc923
Jerome Pinguet <jerome@jerome.cc>
2021-03-02 04:51:47 +0300

    improve timeline admin

--- src/timeline/admin.py
+++ src/timeline/admin.py
@@ -1,11 +1,40 @@
 from django.contrib import admin
 
 from .models import Status
+from conversation.admin_tags import admin_tag_status_datetime
 
 class StatusAdmin(admin.ModelAdmin):
-    list_display = ('statusid', 'status_url_tag', 'community')
-    search_fields = ['statusid',]
-    fields = ('statusid', 'status_url_tag', 'json', 'community',)
-    readonly_fields = ('statusid', 'status_url_tag', 'json', 'community',)
+    list_display = (
+        'statusid',
+        'status_datetime',
+        'status_url_tag',
+        'community'
+    )
+    search_fields = (
+        'statusid',
+    )
+    list_filter = (
+        'community',
+    )
+    fields = (
+        'statusid',
+        'status_datetime',
+        'status_url_tag',
+        'json',
+        'community',
+    )
+    readonly_fields = (
+        'statusid',
+        'status_datetime',
+        'status_url_tag',
+        'json',
+        'community',
+    )
+    
+    def status_datetime(self, obj):
+        return admin_tag_status_datetime(obj.statusid)
+    
+    status_datetime.short_description = 'datetime'
+   
 
 admin.site.register(Status, StatusAdmin)

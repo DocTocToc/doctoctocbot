6078b58b5cef8d8708c812a88b554decccf591c4
Jerome Pinguet <jerome@jerome.cc>
2021-04-20 09:58:59 +0300

    messenger landing pages

--- src/landing/urls.py
+++ src/landing/urls.py
@@ -39,8 +39,19 @@ urlpatterns = [
     path('spamtoctoc/', TemplateView.as_view(template_name='landing/spamtoctoc/index.html'), name='spamtoctoc'),
     path('categories/', TemplateView.as_view(template_name='landing/categories/index.html'), name='categories'),
     path('antiracism/', TemplateView.as_view(template_name='landing/antiracism/index.html'), name='antiracism'),
-
-
-
+    path('messenger/get-started/', TemplateView.as_view(template_name='landing/messenger/get-started.html'), name='messenger-get-started'),
+    path('messenger/features/', TemplateView.as_view(template_name='landing/messenger/features.html'), name='messenger-features'),
+    path('messenger/', TemplateView.as_view(template_name='landing/messenger/index.html'), name='messenger-index'),
+    path('messenger/pricing/', TemplateView.as_view(template_name='landing/messenger/pricing.html'), name='messenger-pricing'),
+    path('messenger/help/', TemplateView.as_view(template_name='landing/messenger/help.html'), name='messenger-help'),
+    path('messenger/open-source/', TemplateView.as_view(template_name='landing/messenger/open-source.html'), name='messenger-open-source'),
+    path('messenger/features/', TemplateView.as_view(template_name='landing/messenger/features.html'), name='messenger-features'),
+    path('messenger/messenger-matrix-store/', TemplateView.as_view(template_name='landing/messenger/messenger-matrix-store.html'), name='messenger-matrix-store'),
+    path('messenger/why-medica/', TemplateView.as_view(template_name='landing/messenger/why-medica.html'), name='messenger-why-medica'),
+    path('messenger/personal/', TemplateView.as_view(template_name='landing/messenger/personal.html'), name='messenger-personal'),
+    path('messenger/communities/', TemplateView.as_view(template_name='landing/messenger/communities.html'), name='messenger-communities'),
+    path('messenger/pro/', TemplateView.as_view(template_name='landing/messenger/pro.html'), name='messenger-pro'),
+    path('messenger/pro/collaboration-federation/', TemplateView.as_view(template_name='landing/messenger/collaboration-federation.html'), name='messenger-collaboration-federation'),
+    path('messenger/linux/', TemplateView.as_view(template_name='landing/messenger/linux.html'), name='messenger-linux'),
 
 ]

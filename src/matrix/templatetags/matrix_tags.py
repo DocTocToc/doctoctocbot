e119375b9e861d6cc802cd7110ae22755ccc266c
Jerome Pinguet <jerome@jerome.cc>
2021-04-23 19:00:10 +0300

    add translaton to matrix templatetags

--- src/matrix/templatetags/matrix_tags.py
+++ src/matrix/templatetags/matrix_tags.py
@@ -1,5 +1,5 @@
 import logging
-
+from community.helpers import activate_language, get_community
 from django import template
 from django.db.utils import DatabaseError
 
@@ -9,13 +9,15 @@ logger = logging.getLogger(__name__)
 
 register = template.Library()
 
-@register.inclusion_tag('matrix/category_ul.html')
-def matrix_category_lst():
+@register.inclusion_tag('matrix/category_ul.html', takes_context=True)
+def matrix_category_lst(context):
+    community = get_community(context.request)
+    activate_language(community)
     try:
         category_lst= list(
             CategoryAccessControl.objects.filter(
                 authorize=True,
-            ).values_list('category__name', flat=True).distinct().order_by()
+            ).values_list('category__label', flat=True).distinct().order_by()
         )
     except DatabaseError:
         category_lst=None

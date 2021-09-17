43dd8cbba23ff599c78ef7f2b00559d2fb81d67c
Jerome Pinguet <jerome@jerome.cc>
2021-09-17 03:45:27 +0300

    add handle_add_friends_to_socialusers task

--- src/community/tasks.py
+++ src/community/tasks.py
@@ -5,7 +5,7 @@ import time
 from community.models import Community
 from bot.bin.friendship import create_friendship_members
 from community.helpers import get_community_bot_socialuser
-from moderation.models import Follower
+from moderation.models import Follower, Friend
 from moderation.profile import create_twitter_social_user
 from celery import shared_task
 
@@ -39,4 +39,14 @@ def handle_add_followers_to_socialusers():
             continue
         bot_follower_id = Follower.objects.filter(user=bot_su).latest().id_list
         for userid in bot_follower_id:
+            create_twitter_social_user(userid)
+
+@shared_task
+def handle_add_friends_to_socialusers():
+    for community in Community.objects.filter(active=True):
+        bot_su = get_community_bot_socialuser(community)
+        if not bot_su:
+            continue
+        bot_friend_id = Friend.objects.filter(user=bot_su).latest().id_list
+        for userid in bot_friend_id:
             create_twitter_social_user(userid)

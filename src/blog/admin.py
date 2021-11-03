826079cdeeed45d21e8cba0cdefb4205a2f9fc36
Jerome Pinguet <jerome@jerome.cc>
2021-11-03 21:25:18 +0300

    remove dangling blog dir

--- src/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import BlogPage
-
-
-class BlogPageAdmin(admin.ModelAdmin):
-    autocomplete_fields = ['authors']
-
-admin.site.register(BlogPage, BlogPageAdmin)

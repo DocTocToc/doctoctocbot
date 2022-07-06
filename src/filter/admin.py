92bab2fd1580756a3cfbf7b8fdc550c1d68e5907
Jerome Pinguet <jerome@jerome.cc>
2022-07-06 18:55:13 +0300

    improve Filter admin display lists

--- src/filter/admin.py
+++ src/filter/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from filter.models import Word, Filter
+from moderation.admin_tags import m2m_field_tag
 
 class WordAdmin(admin.ModelAdmin):
     list_display = (
         'name',
+        'word',
     )
 
 
@@ -11,9 +13,21 @@ class FilterAdmin(admin.ModelAdmin):
     list_display = (
         'name',
         'active',
+        'word_tag',
         'expression',
+        'community_tag',
     )
+
+    def word_tag(self, obj):
+        return m2m_field_tag(obj.word)
+
+    word_tag.short_description = 'Word'
+
+    def community_tag(self, obj):
+        return m2m_field_tag(obj.community)
+
+    community_tag.short_description = 'Community'
+
 
 admin.site.register(Word, WordAdmin)
 admin.site.register(Filter, FilterAdmin)
-

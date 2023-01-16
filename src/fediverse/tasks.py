7ff4511d3605c521ef1f8bd014bb7d75323193b1
Jerome Pinguet <jerome@jerome.cc>
2023-01-16 07:45:47 +0300

    mastodon social graph tasks

--- src/fediverse/tasks.py
+++ src/fediverse/tasks.py
@@ -2,6 +2,7 @@ import logging
 from celery import shared_task
 from community.models import Community
 from fediverse.bot import TootProcessor
+from fediverse.social import record_friends, record_followers
 
 logger = logging.getLogger(__name__)
 
@@ -14,3 +15,21 @@ def handle_triage_toot(pk: int, community: str):
     tp=TootProcessor(pk, community)
     res = tp.triage()
     logger.debug(res)
+
+@shared_task
+def handle_mastodon_all_communities_friends():
+    for c in Community.objects.filter(
+        mastodon_account__isnull=False,
+        mastodon_access__isnull=False,
+        active=True,
+    ):
+        record_friends(c.mastodon_account, c.mastodon_access)
+
+@shared_task
+def handle_mastodon_all_communities_followers():
+    for c in Community.objects.filter(
+        mastodon_account__isnull=False,
+        mastodon_access__isnull=False,
+        active=True,
+    ):
+        record_followers(c.mastodon_account, c.mastodon_access)
